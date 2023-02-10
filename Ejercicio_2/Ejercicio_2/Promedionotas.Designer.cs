0c39795115c3bf95d09cf1f329d6cbc67874f9e5
Michelle2401 <116472057+Michelle2401@users.noreply.github.com>
2023-02-10 15:34:51 -0600

    promedio de notas
    .

---
+++ Ejercicio_2/Ejercicio_2/Promedionotas.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Ejercicio_2
+{
+    partial class Promedionotas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ingresonotaslabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Calcularbutton1 = new System.Windows.Forms.Button();
+            this.Limpiarbutton2 = new System.Windows.Forms.Button();
+            this.nota1textBox = new System.Windows.Forms.TextBox();
+            this.nota2textBox = new System.Windows.Forms.TextBox();
+            this.nota3textBox = new System.Windows.Forms.TextBox();
+            this.nota4textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ingresonotaslabel
+            // 
+            this.ingresonotaslabel.AutoSize = true;
+            this.ingresonotaslabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ingresonotaslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ingresonotaslabel.Location = new System.Drawing.Point(133, 27);
+            this.ingresonotaslabel.Name = "ingresonotaslabel";
+            this.ingresonotaslabel.Size = new System.Drawing.Size(142, 18);
+            this.ingresonotaslabel.TabIndex = 0;
+            this.ingresonotaslabel.Text = "Ingrese las notas:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nota 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nota 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nota 4";
+            // 
+            // Calcularbutton1
+            // 
+            this.Calcularbutton1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Calcularbutton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calcularbutton1.Location = new System.Drawing.Point(60, 308);
+            this.Calcularbutton1.Name = "Calcularbutton1";
+            this.Calcularbutton1.Size = new System.Drawing.Size(87, 64);
+            this.Calcularbutton1.TabIndex = 9;
+            this.Calcularbutton1.Text = "Calcular";
+            this.Calcularbutton1.UseVisualStyleBackColor = false;
+            this.Calcularbutton1.Click += new System.EventHandler(this.Calcularbutton1_Click);
+            // 
+            // Limpiarbutton2
+            // 
+            this.Limpiarbutton2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Limpiarbutton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Limpiarbutton2.Location = new System.Drawing.Point(222, 308);
+            this.Limpiarbutton2.Name = "Limpiarbutton2";
+            this.Limpiarbutton2.Size = new System.Drawing.Size(91, 63);
+            this.Limpiarbutton2.TabIndex = 10;
+            this.Limpiarbutton2.Text = "Limpiar";
+            this.Limpiarbutton2.UseVisualStyleBackColor = false;
+            this.Limpiarbutton2.Click += new System.EventHandler(this.Limpiarbutton2_Click);
+            // 
+            // nota1textBox
+            // 
+            this.nota1textBox.Location = new System.Drawing.Point(136, 75);
+            this.nota1textBox.Name = "nota1textBox";
+            this.nota1textBox.Size = new System.Drawing.Size(71, 21);
+            this.nota1textBox.TabIndex = 11;
+            // 
+            // nota2textBox
+            // 
+            this.nota2textBox.Location = new System.Drawing.Point(136, 116);
+            this.nota2textBox.Name = "nota2textBox";
+            this.nota2textBox.Size = new System.Drawing.Size(71, 21);
+            this.nota2textBox.TabIndex = 12;
+            // 
+            // nota3textBox
+            // 
+            this.nota3textBox.Location = new System.Drawing.Point(136, 162);
+            this.nota3textBox.Name = "nota3textBox";
+            this.nota3textBox.Size = new System.Drawing.Size(71, 21);
+            this.nota3textBox.TabIndex = 13;
+            // 
+            // nota4textBox
+            // 
+            this.nota4textBox.Location = new System.Drawing.Point(136, 222);
+            this.nota4textBox.Name = "nota4textBox";
+            this.nota4textBox.Size = new System.Drawing.Size(71, 21);
+            this.nota4textBox.TabIndex = 14;
+            // 
+            // Promedionotas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 537);
+            this.Controls.Add(this.nota4textBox);
+            this.Controls.Add(this.nota3textBox);
+            this.Controls.Add(this.nota2textBox);
+            this.Controls.Add(this.nota1textBox);
+            this.Controls.Add(this.Limpiarbutton2);
+            this.Controls.Add(this.Calcularbutton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ingresonotaslabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Promedionotas";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ingresonotaslabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Calcularbutton1;
+        private System.Windows.Forms.Button Limpiarbutton2;
+        private System.Windows.Forms.TextBox nota1textBox;
+        private System.Windows.Forms.TextBox nota2textBox;
+        private System.Windows.Forms.TextBox nota3textBox;
+        private System.Windows.Forms.TextBox nota4textBox;
+    }
+}
+
---
+++ Ejercicio_2/Ejercicio_2/Promedionotas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2
+{
+    public partial class Promedionotas : Form
+    {
+        public Promedionotas()
+        {
+            InitializeComponent();
+        }
+
+        private async void Calcularbutton1_Click(object sender, EventArgs e)
+        {
+            decimal nota1 = Convert.ToDecimal(nota1textBox.Text);
+            decimal nota2 = Convert.ToDecimal(nota2textBox.Text);
+            decimal nota3= Convert.ToDecimal(nota3textBox.Text);
+            decimal nota4 = Convert.ToDecimal(nota4textBox.Text);
+
+            decimal promedio = await SumarnotasAsync(nota1, nota2, nota3, nota4);
+
+            MessageBox.Show($"El promedio final es:{promedio}");
+        }
+        private async Task<decimal> SumarnotasAsync(decimal n1, decimal n2, decimal n3, decimal n4)
+        {
+            decimal suma = await Task.Run(() =>
+            {
+                return (n1 + n2 + n3 + n4) / 4;
+            });
+            return suma;
+
+
+        }
+
+        private void Limpiarbutton2_Click(object sender, EventArgs e)
+        {
+          nota1textBox.Clear();
+          nota2textBox.Clear();
+          nota3textBox.Clear();
+          nota4textBox.Clear();
+        }
+
+        
+
+
+    }
+}
